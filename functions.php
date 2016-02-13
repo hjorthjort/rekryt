28460af5b8846eac44713eba846316a6feb2376f
hjorthjort <rikard.hjort@gmail.com>
2016-02-13 16:27:21 +0100

    Favicon

--- functions.php
+++ functions.php
@@ -3,5 +3,10 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
+// Custom Function to Include
+function favicon_link() {
+    echo '<link rel="shortcut icon" type="image/x-icon" href="/rekryt/wp-content/themes/rekryt/resources/logo_minimal.png" />' . "\n";
+}
+add_action( 'wp_head', 'favicon_link' );
 }
 ?>
